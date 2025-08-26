2d93a984bfcc3fa812d99509c1bfe5d424cb2147
adriangui <80858782+adriangui@users.noreply.github.com>
2025-08-25 21:13:15 -0300

    Add files via upload

--- captions.es.bull.js
+++ captions.es.bull.js
@@ -1,9 +1,9 @@
 // captions.es.bull.js — captions por imagen (alcista)
 export const CAPTIONS_ES_BULL = {
   1: ["_"],
-  2: ["Marcamos Docking y Rocking", "Ojo, para que sea válido tiene que haber un mínimo de 5 velas entre el docking y el rocking"],
-  3: ["Marcamos el vórtice", "Ojo, cuidar que la mecha de la parte superior del vórtice no sea más alta que el máximo (este punto lo tengo que afinar bien, por que no estoy seguro cómo es)"],
+  2: ["Marcamos Docking y Rocking", "Ojo, para que sea válido tiene que haber un mínimo de 5 velas entre el docking y el rocking"], //paso 2.png y paso 2b.png
+  3: ["Marcamos el vórtice", "Validar que el vórtice no traspase el nivel del rocking. (Acá tengo una duda, si lo traspasa, ¿puede seguir buscandose un vórtice anterior?)", "También: Ojo, cuidar que la mecha de la parte superior del vórtice no sea más alta que el máximo (este punto lo tengo que afinar bien, por que no estoy seguro cómo es)"],
   4: ["Marcamos el primer máximo", "_"],
-  5: ["Localizar el origen del impulso", "_"],
+  5: ["Localizar el origen del impulso", "Validar que sea más bajo que el vórtice", "_"],
   6: ["_"]
 };
