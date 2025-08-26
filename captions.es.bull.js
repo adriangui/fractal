bfcbbf40d37c8649f91d459c923c0f3b4de07f29
adriangui <80858782+adriangui@users.noreply.github.com>
2025-08-25 21:30:40 -0300

    Add files via upload

--- captions.es.bull.js
+++ captions.es.bull.js
@@ -3,7 +3,7 @@ export const CAPTIONS_ES_BULL = {
   1: ["_"],
   2: ["Marcamos Docking y Rocking", "Ojo, para que sea válido tiene que haber un mínimo de 5 velas entre el docking y el rocking"], //paso 2.png y paso 2b.png
   3: ["Desde el pivote, contamos las velas que suben, hasta que una BAJE", "_"],
-  4: ["Marcamos el primer máximo", "_"],
+  4: ["Marcamos el primer máximo, trazando una línea hasta la primer vela posterior que lo rompa", "Y marcamos el vórtice en el mínimo más bajo de las velas que estén entre estos 2 puntos"],
   5: ["Localizar el origen del impulso", "Validar que sea más bajo que el vórtice", "_"],
   6: ["_"]
 };
