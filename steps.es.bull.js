c8c1d76aa6ace273a8e0c7067cb69405f4cec80b
adriangui <80858782+adriangui@users.noreply.github.com>
2025-08-25 21:15:53 -0300

    Add files via upload

--- steps.es.bull.js
+++ steps.es.bull.js
@@ -5,16 +5,17 @@ export const STEPS_ES_BULL = [
 
   { text: 'Encontrar un posible docking y rocking. Buscar una forma de "V" en donde el segundo movimiento llegue o supere el máximo del primer movimiento, y marcar docking y rocking "potenciales"' },
   
+ 
+  { text: 'Partiendo desde el docking, y yendo para atrás, ir leyendo los movimientos del máximo de cada vela, de esta manera: El primero seguramente "baja". Ir a la siguiente, hasta que "suba". En la imagen de ejemplo hace: "baja", "baja", "baja","baja", "baja", "baja","SUBE" (baja 6 veces, y sube). ' },
   
-  
-  { text: 'Partiendo desde el docking, y yendo para atrás, ir leyendo los movimientos del máximo de cada vela, de esta manera: El primero seguramente "baja". Ir a la siguiente, hasta que "suba". En la imagen de ejemplo hace: "baja", "baja", "baja","baja", "baja", "baja","SUBE" (baja 6 veces, y sube). Cuando sube, en el último que bajó, está el vórtice. Marcar el vórtice en el mínimo de esa vela.' },
-  
-
-  
-  { text: 'Ahora, para detectar el primer máximo, hay que partir de la vela de vórtice, e ir leyendo los máximos que "suben", hasta que "baje". En el ejemplo tenemos que: "Sube, sube, baja". El máximo del último "sube", es el primer máximo.' },
+  // { text: 'Partiendo desde el docking, y yendo para atrás, ir leyendo los movimientos del máximo de cada vela, de esta manera: El primero seguramente "baja". Ir a la siguiente, hasta que "suba". En la imagen de ejemplo hace: "baja", "baja", "baja","baja", "baja", "baja","SUBE" (baja 6 veces, y sube). Cuando sube, en el último que bajó, está el vórtice. Marcar el vórtice en el mínimo de esa vela.' },
   
 
   
+  // { text: 'Ahora, para detectar el primer máximo, hay que partir de la vela de vórtice, e ir leyendo los máximos que "suben", hasta que "baje". En el ejemplo tenemos que: "Sube, sube, baja". El máximo del último "sube", es el primer máximo.' },
+  
+
+  { text: 'Cuando sube, en el último que bajó, marcamos un pivote. Luego seguir los movimientos hacia atras hasta que baje (en el ejemplo hace "sube, sube, BAJA". El último que sube es el primer máximo. Buscamos el vórtice entre la vela del primer máximo y la vela posterior que lo rompe' },  
   
   { text: 'Para ver el origen del movimiento, alargar la línea del primer máximo hacia atras hasta que se choque con una vela. Luego localizar el mínimo más bajo entre el primer máximo y esa vela' },
   
