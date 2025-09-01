153eb3857209d623307aa45cc060509c2a240d86
adriangui <80858782+adriangui@users.noreply.github.com>
2025-09-01 17:58:35 -0300

    Add files via upload

--- steps.es.bull.js
+++ steps.es.bull.js
@@ -1,8 +1,8 @@
 /// steps.es.bull.js
 export const STEPS_ES_BULL = [
-  { text: 'Primero veamos como se ve este fractal alcista resuelto' }, //1
+  { text: 'Mira el video de explicación del algoritmo <a href="https://youtu.be/IHsGLuw6XHI" target="_blank">aquí</a>.' }, //1
   { text: 'Buscar una forma de "V" en donde el segundo movimiento llegue o supere el máximo del primer movimiento. Marcar docking y rocking "potenciales", validando que haya al menos 5 velas entre el docking y el rocking' }, //2
-  { text: 'Partiendo desde el docking, y yendo para atrás, ir leyendo los movimientos del máximo de cada vela, contando los que bajen, hasta que SUBE, para marcar un pivote temporal' }, //3
+  { text: 'Partiendo desde el docking, y yendo para atrás, ir leyendo los movimientos del máximo de cada vela, contando los que bajen, hasta que SUBA, para marcar un pivote temporal' }, //3
    { text: 'En la última vela que bajó, marcar un pivote temporal' }, //4
   { text: 'Desde el pivote, yendo para atrás, contamos las velas que suban en sus máximos, hasta que una BAJE, para buscar el primer máximo' }, //5
   { text: 'Marcar el "primer máximo" en el alto de la última vela que subió. Atención: Es válido solo si es más bajo que el docking' }, //6
