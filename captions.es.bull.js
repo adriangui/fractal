e5c37aa7ac437eee500196b7dc7c2bf5f82ec64e
adriangui <80858782+adriangui@users.noreply.github.com>
2025-09-01 16:35:09 -0300

    Add files via upload

--- captions.es.bull.js
+++ captions.es.bull.js
@@ -1,9 +1,13 @@
-// captions.es.bull.js — captions por imagen (alcista)
+// captions.es.bull.js 
 export const CAPTIONS_ES_BULL = {
   1: ["_"],
-  2: ["Marcamos Docking y Rocking", "Ojo, para que sea válido tiene que haber un mínimo de 5 velas entre el docking y el rocking"], //paso 2.png y paso 2b.png
-  3: ["Desde el pivote, contamos las velas que suben, hasta que una BAJE", "_"],
-  4: ["Marcamos el primer máximo, trazando una línea hasta la primer vela posterior que lo rompa. Y marcamos el vórtice en el mínimo más bajo de las velas que estén entre estos 2 puntos", "_"],
-  5: ["Localizar el origen del impulso", "Validar que sea más bajo que el vórtice", "_"],
-  6: ["_"]
+  2: ["Marcar docking y rocking potenciales", "_"],
+  3: ["_"],
+  4: ["_"],
+  5: ["_"],
+  6: ["_"],
+  7: ["_"],
+  8: ["_"],
+  9: ["_"],
+  10: ["_"],
 };
--- steps.es.bull.js
+++ steps.es.bull.js
@@ -1,27 +1,14 @@
-// steps.es.bull.js (alcista) — según tu versión más reciente + paso 10 de validación
-export const STEPS_ES_BULL = [
-
- { text: 'Primero veamos como se ve este fractal alcista resuelto' },
-
-  { text: 'Encontrar un posible docking y rocking. Buscar una forma de "V" en donde el segundo movimiento llegue o supere el máximo del primer movimiento, y marcar docking y rocking "potenciales"' },
-  
- 
-  { text: 'Partiendo desde el docking, y yendo para atrás, ir leyendo los movimientos del máximo de cada vela, contando los que bajan, hasta que SUBA, y en la última vela que bajó, marcar un pivote temporalmente. ' },
-  
-  // { text: 'Partiendo desde el docking, y yendo para atrás, ir leyendo los movimientos del máximo de cada vela, de esta manera: El primero seguramente "baja". Ir a la siguiente, hasta que "suba". En la imagen de ejemplo hace: "baja", "baja", "baja","baja", "baja", "baja","SUBE" (baja 6 veces, y sube). Cuando sube, en el último que bajó, está el vórtice. Marcar el vórtice en el mínimo de esa vela.' },
-  
-
-  
-  // { text: 'Ahora, para detectar el primer máximo, hay que partir de la vela de vórtice, e ir leyendo los máximos que "suben", hasta que "baje". En el ejemplo tenemos que: "Sube, sube, baja". El máximo del último "sube", es el primer máximo.' },
-  
-
-  { text: 'Marcar el "primer máximo" en el alto de la última vela que subió. Atención: Es válido solo si es más bajo que el docking'},  
-  
-  { text: 'Para ver el origen del movimiento, alargar la línea del primer máximo hacia atras hasta que se choque con una vela. Luego localizar el mínimo más bajo entre el primer máximo y esa vela' },
-  
-    //////////////////////////// 
-  
-  
- 
-  { text: 'Al trazar los puntos, queda resuelto' }
+/// steps.es.bull.js
+export const STEPS_ES_BEAR = [
+  { text: 'Primero veamos como se ve este fractal alcista resuelto' }, //1
+  { text: 'Buscar una forma de "V" en donde el segundo movimiento llegue o supere el máximo del primer movimiento. Marcar docking y rocking "potenciales", validando que haya al menos 5 velas entre el docking y el rocking' }, //2
+  { text: 'Partiendo desde el docking, y yendo para atrás, ir leyendo los movimientos del máximo de cada vela, contando los que bajen, hasta que SUBE, para marcar un pivote temporal' }, //3
+   { text: 'En la última vela que bajó, marcar un pivote temporal' }, //4
+  { text: 'Desde el pivote, yendo para atrás, contamos las velas que suban en sus máximos, hasta que una BAJE, para buscar el primer máximo' }, //5
+  { text: 'Marcar el "primer máximo" en el alto de la última vela que subió. Atención: Es válido solo si es más bajo que el docking' }, //6
+  { text: 'Trazamos una línea desde el primer máximo hasta la primer vela posterior que lo rompa de abajo hacia arriba, para buscar el vórtice.' }, //7
+  { text: 'Marcamos el vórtice en el mínimo más bajo de las velas que estén entre estos 2 puntos. Muchas veces el pivote temporal se convierte en el vórtice.' }, //8
+  { text: 'Localizar origen del impulso y validar que sea más bajo que el vórtice y el rocking.' }, //9
+  { text: 'Al trazar los puntos, queda resuelto' } //10
 ];
+
