ae95049b17f9c0308db45df95079feb7ecd99f53
adriangui <80858782+adriangui@users.noreply.github.com>
2025-09-01 16:31:31 -0300

    Add files via upload

--- images.bull.js
+++ images.bull.js
@@ -1,13 +1,13 @@
 // images.bull.js —
 export const STEP_IMAGES_BULL = {
   1: ["fractal alcista resuelto algoritmo.png"],
-  2: ["paso 2 alcista.png", "paso 2 b alcista.png"],
-  3: ["paso 3 alcista.png"],
-  4: ["paso 4 alcista.png"],
-  5: ["paso 5 alcista.png"],
-  6: ["paso 6 alcista.png"],
-  7: ["paso 7 alcista.png"],
-  8: ["paso 8 alcista.png"],
-  9: ["paso 9 alcista.png"],
+  2: ["paso 2.png", "paso 2 b.png"],
+  3: ["paso 3.png"],
+  4: ["paso 4.png"],
+  5: ["paso 5.png"],
+  6: ["paso 6.png"],
+  7: ["paso 7.png"],
+  8: ["paso 8.png"],
+  9: ["paso 9.png"],
   10: ["fractal alcista resuelto algoritmo.png"],  
 };
